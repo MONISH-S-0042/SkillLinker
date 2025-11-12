2426949c813e296bd3d2a353d0a7faa606001759
MONISH S <smonish18012006@gmail.com>
2025-11-12 21:05:27 +0530

    Use environment variable for server port

--- app.js
+++ app.js
@@ -206,6 +206,6 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = "Something Went Wrong!!"
     res.status(statusCode).render('error', { err });
 })
-server.listen(3000, () => {
+server.listen(process.env.PORT ||3000, () => {
     console.log("Listening on port 3000");
 });
